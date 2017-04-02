3925f52816ad5012d5e6d5700ae81ecbb7260453
ajet <cbibop.xo@gmail.com>
2017-04-02 18:11:12 +0300

    Готово задание осталось только проверить запись в файл

--- words_statistics.py
+++ words_statistics.py
@@ -32,12 +32,20 @@ def _calc_stats(words):
     words: words list
     """
     frame = pd.DataFrame(words, columns=['word'])
-    total = frame.count().word
     out = []
     for unique in frame.word.unique():
-        out.append((unique, (frame[frame.word == unique].count().values[0])/total))
+        out.append((unique, (frame[frame.word == unique].count().values[0])))
 
     return out
+
+def _save_result(result, f_name=None):
+    """
+    save file
+    result: list of tupels
+    """
+    result = pd.DataFrame(result, columns=['word', 'freq'])
+    result = result.sort_values(['freq', 'word'], ascending=[False, True])
+    return result.to_csv(f_name, sep=':', header=False, index=False)
 
 
 def _run_reading_and_calc(arg):
@@ -58,10 +66,7 @@ def _run_reading_and_calc(arg):
         msg = 'Check file format in  {}'.format(arg)
         raise  IOError(msg)
 
-    result = pd.DataFrame(_calc_stats(_get_words_from_file(in_put)),\
-        columns=['word', 'freq'])
-
-    print(result.word.sort)
+    _save_result(_calc_stats(_get_words_from_file(in_put)), '')
 
 
 def _run_unittests():
@@ -82,6 +87,8 @@ def _run_unittests():
             self._short_text = 'aaaa, bbbb'
             self._short_file = StringIO(self._short_text)
             self._words = ['aaaa', 'bbbb']
+            self._result = [('ab', 3), ('ac', 2), ('b', 4), ('c', 1)]
+            self._output_format = "b:4\nab:3\nac:2\nc:1\n"
 
         def test_a(self):
             """
@@ -95,7 +102,13 @@ def _run_unittests():
             """
             result = _calc_stats(self._words)
             for res in result:
-                self.assertEqual(res[1], 0.5)
+                self.assertEqual(res[1], 1)
+
+        def test_c(self):
+            """
+            Save result
+            """
+            self.assertEqual(_save_result(self._result), self._output_format)
 
     suite = unittest.TestSuite()
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestCounter))
@@ -115,10 +128,10 @@ def _read_args_and_run():
     if (n_arg > 1) & (n_arg <= 3):
 
         if (n_arg == 2) & ('--run_self_test' in arg[1]):
-            _run_reading_and_calc(arg[1])
+            _run_unittests()
 
         elif (n_arg == 3) & ('--get_data' in arg[1]):
-            _get_words_from_file(arg[2])
+            _run_reading_and_calc(arg[2])
 
         else:
             print(help_msg)
